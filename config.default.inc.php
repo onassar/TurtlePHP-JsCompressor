3ed09c119c13544452e9458121382d18cdde9fdc
Oliver Nassar <onassar@gmail.com>
2016-03-24 18:16:04 -0400

    Minor updates to pathing (not that important)

--- config.default.inc.php
+++ config.default.inc.php
@@ -37,12 +37,12 @@
     // Batches of files to compressor and write
     $batches = array(
         'app' => array(
-            'storage' => WEBROOT . '/static/app/js/compiled',
+            'storage' => WEBROOT . '/app/static/js/compiled',
             'files' => array(
 
                 // Dependencies
-                WEBROOT . '/static/app/vendors/extend.js',
-                WEBROOT . '/static/app/vendors/jquery-1.11.1.min.js'
+                WEBROOT . '/app/static/vendors/extend.js',
+                WEBROOT . '/app/static/vendors/jquery-1.11.1.min.js'
             )
         )
     );
