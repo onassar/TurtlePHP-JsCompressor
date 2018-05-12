2c3e141c23dd8b6d54b3a77c080ec8087a88e99b
onassar <onassar@gmail.com>
2018-05-12 18:24:27 -0400

    Cleanup

--- config.default.inc.php
+++ config.default.inc.php
@@ -41,8 +41,8 @@
             'files' => array(
 
                 // Dependencies
-                WEBROOT . '/app/static/vendors/extend.js',
-                WEBROOT . '/app/static/vendors/jquery-1.11.1.min.js'
+                WEBROOT . '/app/static/vendors/external/extend/v0.0.0/extend.js',
+                WEBROOT . '/app/static/vendors/external/jquery/v1.11.3/jQuery.min.js'
             )
         )
     );
